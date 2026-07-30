320edc04dcc273c0a2704f8ae5de4965770eea88
Veronica Moreno <veromr@gmail.com>
2026-07-30 18:06:01 -0300

    //AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,17 +1,19 @@
 import { test, expect } from '@playwright/test'
 
+///AAA - Arrange, Act, Assert
+
+///Arrange
 test('deve consultar um pedido aprovado', async ({ page }) => {
   await page.goto('http://localhost:5173/')
-//Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-//Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+  
+///Act
   await page.getByTestId('search-order-id').fill('VLO-X2T435')
-
   await page.getByTestId('search-order-button').click()
+
+///Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-X2T435')
   await expect(page.getByTestId('order-result-status')).toBeVisible()
